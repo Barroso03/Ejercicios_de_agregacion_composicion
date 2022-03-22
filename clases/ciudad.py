5cd7215006a95fdd46f5f5f97864841b9c321c07
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-03-22 12:12:06 +0100

    creamos el constructor

--- clases/ciudad.py
+++ clases/ciudad.py
@@ -1,2 +1,6 @@
-class ciudad():
-  def __init__(self,ciudad)
+class catastrofe():
+  def __init__(self, empresa, empleado, ciudad, edificio):
+    self.empresa = empresa
+    self.empleado = empleado
+    self.ciudad = ciudad
+    self.edidificio = edificio
