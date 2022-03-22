243c85e4509bc235e445887898226870f9777ece
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-03-22 13:07:49 +0000

    Al final he creado solo una clase llamada destruccion

---
+++ clases/destruccion.py
@@ -0,0 +1,18 @@
+class destruccion():
+  def __init__(self, ciudades):
+    self.ciudades = ciudades
+  def ciudad(New_York):
+   while True:
+     print("Que ciudad quieres destruir")
+     eleccion = str(input("Elige una ciudad entre New_York o Los_Angeles:"))
+     break
+   if eleccion == New_York:
+     print("Se ha destruido New_York")
+     print("Se ha destruido los edificios A y B")
+     print("Mueren Sr.Salim y Sr.Martin ")
+   else:
+     print("Se ha destruido Los_Angeles")
+     print("Se ha destruido el edificio C")
+     print("Muere Sra.Xing ")
+print(destruccion.ciudad("New_York"))
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from clases.destruccion import destruccion 
+
+
+if __name__ == "__main__":
+  print(destruccion.ciudad("New_York"))
+  
+  
+  
